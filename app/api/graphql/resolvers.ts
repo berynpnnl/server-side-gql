e6eae075df9a8df140e3b64f7ca0a37ad81e562b
beryn.staub-waldenberg@pnnl.gov <beryn.staub-waldenberg@pnnl.gov>
2024-10-30 17:49:56 -0700

    saving progress from demo

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -1,6 +1,36 @@
 const resolvers = {
   Query: {
-    user: (parent, args, context, info) => {},
+    me: () => {
+      return 'hullo'
+    },
+    people: () => {
+      return [{ name: 'samie', id: 1234, pets: [1, 2] }]
+    },
+    search: () => {
+      return [
+        { name: 'samuel', id: 0 },
+        { name: 'fido', species: 'doge' },
+      ]
+    },
+  },
+  Person: {
+    name: (p) => p.name.toUpperCase(),
+    pets: (person) => [
+      { name: 'catty', species: 'cat' },
+      { name: 'dogy', species: 'doge' },
+    ],
+  },
+  COLORS: {
+    BLUE: '#3902',
+  },
+  SearchType: {
+    __resolveType: (b) => {
+      if (b.species) {
+        return 'Animal'
+      } else {
+        return 'Person'
+      }
+    },
   },
 }
 
--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,78 +1,96 @@
 const schema = `#graphql
-
-  interface Character {
-    name: String!
-    outfit: String!
-    strengthStat: Int!
-  }
-
-
-
-  type Person implements Character {
-    name: String!
-    outfit: String!
-    strengthStat: Int!
-
-    backgroundStory: String
-    profile: Profile!
-  }
-
-  type Alien implements Character {
-    name: String!
-    outfit: String!
-    strengthStat: Int!
-
-    homeWorld: String
-  }
-
-  input SearchInput {
-    id: ID
-    name: String
-  }
-
-
-
-  type Tweet {
-    id: ID!
-    content: String!
-  }
-
-  type Profile {
-    id: ID!
-    username: String! @deprecated(reason: "this is an old field that's no longer used")
-    person: Person! 
-  }
-
-
-  union SearchResult = Tweet | Profile
-
-
-  enum STATUS {
-    IN_PROGRESS
-    DONE
-    TODO
-  }
-
-  type Todo {
-    status: STATUS
-  }
-
-  # for getting stuff
   type Query {
-    me: Person!
-    characters: [Character!]!
-    search(input: SearchInput!): [SearchResult]!
+    me: String!
+    people: [Person!]!
+    search: [SearchType]!
   }
 
-  # for creating, deleting, and modifying stuff
-  type Mutation {
-    makeTweet(content: String!): Tweet!
+  type Person {
+    name: String!
+    id: ID!
+    pets: [Animal]!
   }
 
-  type Subscription {
-    userUpdated(id: ID!): User
+  enum COLORS {
+    BLUE
+    RED
+    GREEN
+    BROWN
   }
+
+  type Animal {
+    species: String!
+    name: String!
+  }
+  union SearchType = Animal | Person
 
 `
 
 export default schema
+
+// interface Character {
+//   name: String!
+//   outfit: String!
+//   strengthStat: Int!
+// }
+
+// type Person implements Character {
+//   name: String!
+//   outfit: String!
+//   strengthStat: Int!
+
+//   backgroundStory: String
+//   profile: Profile!
+// }
+
+// type Alien implements Character {
+//   name: String!
+//   outfit: String!
+//   strengthStat: Int!
+
+//   homeWorld: String
+// }
+
+// input SearchInput {
+//   id: ID
+//   name: String
+// }
+
+// type Tweet {
+//   id: ID!
+//   content: String!
+// }
+
+// type Profile {
+//   id: ID!
+//   username: String! @deprecated(reason: "this is an old field that's no longer used")
+//   person: Person!
+// }
+
+// union SearchResult = Tweet | Profile
+
+// enum STATUS {
+//   IN_PROGRESS
+//   DONE
+//   TODO
+// }
+
+// type Todo {
+//   status: STATUS
+// }
+
+// # for getting stuff
+// type Query {
+//   me: Person!
+//   characters: [Character!]!
+//   search(input: SearchInput!): [SearchResult]!
+// }
+
+// # for creating, deleting, and modifying stuff
+// type Mutation {
+//   makeTweet(content: String!): Tweet!
+// }
+
+// type Subscription {
+//   userUpdated(id: ID!): User
+// }
